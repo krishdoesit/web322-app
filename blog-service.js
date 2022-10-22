a5781bef53e649cce6dc82e788579fb63b144282
krishdoesit <khpatel28@myseneca.ca>
2022-10-21 21:37:23 -0400

    final commit for assignment 3

--- blog-service.js
+++ blog-service.js
@@ -1,6 +1,8 @@
 var posts=[];
 var categories=[];
+const { doesNotReject } = require("assert");
 const fs=require("fs");
+const { resolve } = require("path");
 
 exports.initialize=()=>{
     return new Promise((resolve,reject)=>{
@@ -58,5 +60,66 @@ exports.getCategories=()=>{
             resolve(categories);
         }
     })
+}
+
+exports.addPost=(postData)=>{
+    postData.published == undefined ? postData.published=false : postData.published=true;
+    postData.id=posts.length+1;
+    posts.push(postData);
+   // resolve(posts);
+    return new Promise((resolve,reject)=>{
+        if(posts.length==0){
+            reject('no results');
+        }
+        else{
+            resolve(posts);
+        }
+    })
+}
+
+exports.getPostsByCategory=(postcatId)=>{
+   return new Promise((resolve,reject)=>{
+    var pcategoryid=[];
+    for(let i =0; i < posts.length;i++){
+        if(posts[i].category==postcatId){
+            pcategoryid.push(posts[i]);
+        }
+    }
+    if(pcategoryid){
+        resolve(pcategoryid);
+    }else{
+        reject("no results returned");
+    }
+   })
+} 
+
+exports.getPostsByMinDate=(minDateStr)=>{
+    var pdateData=[];
+    return new Promise((resolve,reject)=>{
+      
+        for(let i =0; i < posts.length; i++){
+        if(new Date(posts[i].postDate) >= new Date(minDateStr)){
+            console.log("The postDate value is greater than minDateStr")
+            pdateData.push(posts[i]);
+        }
+        resolve(pdateData);
+    }    
+    })
+}
+
+exports.getPostById=(id)=>{
+  return new Promise((resolve,reject)=>{
+    var post_id;
+    for(let i=0;i<posts.length;i++){
+        if(posts[i].id==id){
+            post_id=posts[i];
+        }
+    }
+    if(post_id){
+        resolve(post_id);
+    }else{
+        reject("id not found");
+    }
+  })
 }
 //
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Krish HarshadKumar Student ID: 123898215 Date: 30/09/2022
+*
+*  Cyclic Web App URL: https://nutty-knickers-fox.cyclic.app/about
+*
+*  GitHub Repository URL: https://github.com/krishdoesit/web322-app
+*
+********************************************************************************/ 
+
 var required= require("./blog-service");
 const express=require("express");
 const { dirname } = require("path");
 const path = require("path");
 var blogservice=require(__dirname+"/blog-service.js")
+const multer=require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+cloudinary.config({
+    cloud_name:'daiwvaoap',
+    api_key:'257675341733921',
+    api_secret:'0rRGUzEKMVHxbATAYbmMMebPSMg',
+    secure:true
+});
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
 
 const { application } = require("express");
+const { readSync } = require("fs");
 var app=express();
 var PORT=process.env.PORT||8080;
 
@@ -32,8 +58,34 @@ app.get('/blog',(req,res)=>{
 });
 
 app.get('/posts',(req,res)=>{
+    if(req.query.category){//query /posts?category
+        blogservice.getPostsByCategory(req.query.category).then((categoryId)=>{
+            console.log("expected run");
+            res.json({categoryId})
+        }).catch((err)=>{
+            res.json({message:err});
+        })
+    }
+    else if(req.query.minDate){//query /posts?minDate
+        blogservice.getPostsByMinDate(req.query.minDate).then((databydate)=>{
+            res.json({databydate})
+        }).catch((err)=>{
+            res.json({message:"error"});
+        })
+    }
+    else{
+        blogservice.getAllPosts().then((data)=>{
+            res.json({data});
+        }).catch((err)=>{
+            res.json({message:err});
+        })
+    }
+});
+
+
+app.get('/posts',(req,res)=>{
     blogservice.getAllPosts().then((data)=>{
-        res.json({data});
+        res.json({data});console.log("/posts running");
     }).catch((err)=>{
        res.json({"message":err})
     })
@@ -46,6 +98,72 @@ app.get('/categories',(req,res)=>{
         res.json({"message":err})
     })
 });
+
+app.get('/posts/add',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/addPost.html"));//sending addPost.html file to route /posts/add
+})
+
+app.post('/posts/add',upload.single("featureImage"),(req,res)=>{
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processPost(uploaded.url);
+        });
+    }else{
+        processPost("");
+    }
+     
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+        blogservice.addPost(req.body).then(()=>{
+        res.redirect("/posts");
+        })
+    
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+        
+    } 
+    
+})
+
+// app.post('/posts/add',(req,res)=>{
+//     blogservice.addPost(req.body).then(()=>{
+//         res.redirect("/posts");
+//     })
+// })
+
+//posts query routes
+
+
+app.get("/posts/:id",(req,res)=>{
+    blogservice.getPostById(req.params.id).then((data)=>{
+        res.json({data});
+    }).catch((err)=>{
+        res.json({message:err});
+    })
+});//working perfectlycd
+
+
 
 app.get('*',(req,res)=>{
     res.status(404).send("Page Not Found");
