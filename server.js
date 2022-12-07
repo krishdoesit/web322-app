46faba2362767e6c73b2ad279582d844b381e9e7
krishdoesit <khpatel28@myseneca.ca>
2022-12-07 01:42:10 -0500

    latest commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*********************************************************************************
-*  WEB322 – Assignment 04
+*  WEB322 – Assignment 05
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
 *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: krish Harshadkumar Patel Student ID: 123898215 Date: 2022/11/06
+*  Name: krish Harshadkumar Patel Student ID: 123898215 Date: 2022/11/21
 *
-*  Cyclic Web App URL: https://nutty-knickers-fox.cyclic.app/
+*  Cyclic Web App URL: https://nutty-knickers-fox.cyclic.app/posts
 * 
-*  GitHub Repository URL: https://github.com/krishdoesit/web322-app
+*  GitHub Repository URL:https://github.com/krishdoesit/web322-app
 *
 ********************************************************************************/
 
@@ -16,6 +16,8 @@
 //var required= require("./blog-service");
 //const { dirname } = require("path");
 //var blogservice=require(__dirname+"/blog-service.js");
+
+const clientSessions = require("client-sessions");
 const express = require("express");
 const path = require("path");
 const blogData = require("./blog-service");
@@ -23,6 +25,7 @@ const multer = require("multer");
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 const stripJs = require('strip-js');
+const authData = require("./auth-service");
 
 
 //cloudinary config//////////////////////////
@@ -82,18 +85,39 @@ app.engine('.hbs', exphbs.engine({
             }
         },
 
-        formatDate: function(dateObj){
+        formatDate: function (dateObj) {
             let year = dateObj.getFullYear();
             let month = (dateObj.getMonth() + 1).toString();
             let day = dateObj.getDate().toString();
-            return `${year}-${month.padStart(2, "0")}-${day.padStart(2,"0" )}`;
+            return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
         }
-        
+
     }
 }));
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(clientSessions({
+    cookieName: "session",
+    secret: "week10example_web322",
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60
+}));
+
+app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+
+function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+};
 
 app.get('/blog', async (req, res) => {
 
@@ -146,7 +170,7 @@ app.get('/blog', async (req, res) => {
 
 });
 
-app.get('/blog/:id', async (req, res) => {
+app.get('/blog/:id',ensureLogin, async (req, res) => {
 
     // Declare an object to store properties for the view
     let viewData = {};
@@ -198,7 +222,7 @@ app.get('/blog/:id', async (req, res) => {
 
 
 
-app.get('/posts', (req, res) => {
+app.get('/posts',ensureLogin, (req, res) => {
     if (req.query.category) {//query /posts?category
         blogData.getPostsByCategory(req.query.category).then((categoryId) => {
             res.render("posts", { posts: categoryId });
@@ -226,7 +250,7 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.get('/posts', (req, res) => {
+app.get('/posts',ensureLogin, (req, res) => {
     blogData.getAllPosts().then((data) => {
         if (data.length > 0) {
             res.render("posts", { posts: data });
@@ -236,53 +260,53 @@ app.get('/posts', (req, res) => {
     })
 });
 
-app.get('/categories', (req, res) => {
+app.get('/categories',ensureLogin, (req, res) => {
     blogData.getCategories().then((data) => {
-        if(data.length>0){
-        res.render("categories", { categories: data })
+        if (data.length > 0) {
+            res.render("categories", { categories: data })
         }
-        else{
-            res.render("categories",{message:"no results"})
+        else {
+            res.render("categories", { message: "no results" })
         }
     })
 });
 
-app.get("/categories/add",(req,res)=>{
-    res.render(path.join(__dirname+"/views/addCategory.hbs"));
+app.get("/categories/add",ensureLogin, (req, res) => {
+    res.render(path.join(__dirname + "/views/addCategory.hbs"));
 });
 
-app.post("/categories/add",(req,res)=>{
-    blogData.addCategory(req.body).then(()=>{
+app.post("/categories/add",ensureLogin, (req, res) => {
+    blogData.addCategory(req.body).then(() => {
         res.redirect("/categories");
     })
 });
 
-app.get("/categories/delete/:id",(req,res)=>{
+app.get("/categories/delete/:id", ensureLogin,(req, res) => {
     blogData.deleteCategoryById(req.params.id)
-    .then(res.redirect("/categories"))
-    .catch(err=>res.status(500).send("unable to remove category"))
+        .then(res.redirect("/categories"))
+        .catch(err => res.status(500).send("unable to remove category"))
 });
 
-app.get("posts/delete/:id",(req,res)=>{
+app.get("posts/delete/:id",ensureLogin, (req, res) => {
     blogData.deletePostById(req.params.id)
-    .then(res.redirect("/posts"))
-    .catch(err=>res.status(500).send("unable to Remove Post"))
+        .then(res.redirect("/posts"))
+        .catch(err => res.status(500).send("unable to Remove Post"))
 });
 
-app.get('/posts/add', (req, res) => {
+app.get('/posts/add',ensureLogin, (req, res) => {
     //res.render("addPost");//sending addPost.html file to route /posts/add
     blogData.getCategories()
-    .then(data=>res.render("addPost",{categories:data}))
-    .catch(err=>res.render("addPost",{categories:[]}))
+        .then(data => res.render("addPost", { categories: data }))
+        .catch(err => res.render("addPost", { categories: [] }))
 });
 
-app.get("/posts/delete/:id",(req,res)=>{
+app.get("/posts/delete/:id",ensureLogin, (req, res) => {
     blogData.deletePostById(req.params.id)
-    .then(res.redirect("/posts"))
-    .catch(err=>res.status(500).send("unable to remove post"))
+        .then(res.redirect("/posts"))
+        .catch(err => res.status(500).send("unable to remove post"))
 });
 
-app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+app.post('/posts/add',ensureLogin, upload.single("featureImage"), (req, res) => {
     if (req.file) {
         let streamUpload = (req) => {
             return new Promise((resolve, reject) => {
@@ -326,7 +350,7 @@ app.post('/posts/add', upload.single("featureImage"), (req, res) => {
 })
 
 
-app.get("/posts/:id", (req, res) => {
+app.get("/posts/:id",ensureLogin, (req, res) => {
     blogData.getPostById(req.params.id).then((data) => {
         res.json({ data });
     }).catch((err) => {
@@ -334,14 +358,57 @@ app.get("/posts/:id", (req, res) => {
     })
 });//working perfectlycd
 
+//login
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+
+app.post("/register",(req,res)=>{
+    authData.registerUser(req.body)
+    .then(()=>res.render("register",{successMessage:"User created"}))
+    .catch(err=>res.render("register",{errorMessage:err,userName:req.body.userName}))
+});
+
+app.post("/login",(req,res)=>{
+    req.body.userAgent = req.get('User-Agent');
+    authData.checkUser(req.body).then((user) => {
+        req.session.user = {
+            userName:user.userName, // authenticated user's userName
+            email: user.email,// authenticated user's email
+            loginHistory: user.loginHistory// authenticated user's loginHistory
+        }
+    
+        res.redirect('/posts');
+    })
+    .catch(err=>{
+        res.render("login",{errorMessage: err, userName: req.body.userName})
+    })    
+});
+
+app.get("/logout",(req,res)=>{
+    req.session.reset();
+    res.redirect("/");
+})
+
+app.get("/userHistory",ensureLogin,(req,res)=>{
+    res.render("userHistory",{user:req.session.user});
+})
+
 
 //rendering 404 error page
 app.get('*', (req, res) => {
     res.render("404");
 })
 
-blogData.initialize().then(() => {
-    app.listen(PORT, onHTTPStart());
-}).catch(() => {
-    console.log("unable to read file");
-});
+
+blogData.initialize()
+    .then(authData.initialize)
+    .then(() => {
+        app.listen(PORT, onHTTPStart());
+    }).catch(() => {
+        console.log("unable to read file");
+    });
